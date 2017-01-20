979023388de63cecfe6ff7ab91986788b649a207
Casey Dailey <casey@feasttogether.org>
2017-01-19 22:13:19 -0600

    got the js working

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
+Use JavaScript arrays, loops, and innerHTML to show the music you love.
 
-    
+Students must use JavaScript to create a list of songs in the index.html file for their Music History project. Have them download the songs.js file, which contains an array of strings with song information.
 
-    Create five options for the artist select element of any artist that you enjoy.
-    Create at least five options for the album select element. One, or more, album for each artist.
-    The links in the navigation bar don't need to link to anything just yet, you can use <a href="#">View music</a> for now
-    Pick your four favorite songs from the artists you have chosen and use the information for each in the list that's on the right-hand side. You can use h1 tags, div tags, section tags... whatever you like.
+    Each student must add one song to the beginning and the end of the array.
+    Loop over the array and remove any words or characters that obviously don't belong.
+    Students must find and replace the > character in each item with a - character.
 
+    Must add each string to the DOM in index.html in the main content area.
---
+++ javascripts/songs.js
@@ -0,0 +1,133 @@
+//use JavaScript to create a list of songs in the index.html
+
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+//add one song to the beginning and the end of the array.
+
+console.log('songs:', songs);
+
+//to the beginning:
+songs.unshift("Tokyo - by The Books on the album The Lemon of Pink");
+console.log('to the beginning', songs);
+
+//to the end:
+songs.push("There is no there - by The Books on the album The Lemon of Pink");
+console.log('to the end:', songs);
+
+
+//Loop over the array and remove any words or characters that obviously don't belong.
+
+
+var cleanSongs = []
+
+for (var i = 0; i < songs.length; i++) {
+
+	//i can't get this to work right now. i know its gotta be something like this
+	
+	// var	cleanSong = songs[i].replace((\/>\/g), "-");
+		// console.log('songs[i] - ">" + "-":', songs[i]);
+	// var gibberish = (/@|!|\(|*\/)g;
+		// songs[i] = cleanSong.replace(gibberish, "");
+		// console.log('song[i] - minus gibberish:', songs[i]);
+	
+	// var dashSong = songs[i].replace(">", "-");
+	// console.log('dashSong', dashSong);
+	// cleanSong = dashSong.replace("*", "");
+	// console.log('cleanSong:', cleanSong);
+	// cleanSong = cleanSong.replace("!", "");
+	// console.log('cleanSong:', cleanSong);
+	// cleanSong = cleanSong.replace("(", "");
+	// console.log('cleanSong:', cleanSong);
+	// cleanSong = cleanSong.replace("@", "");
+	// console.log('cleanSong:', cleanSong);
+
+//hours later, without the escape '\' on the '>' 
+//and without the parentheses on	gibberish
+
+			var dashSong = songs[i].replace(/>/g, "-");
+			var gibberish = /\(|@|!|\*|/g;
+			var cleanSong = dashSong.replace(gibberish, ""); 
+			console.log(cleanSong); 
+
+	cleanSongs.push(cleanSong);
+
+};
+
+console.log(cleanSongs); 
+
+//finallY! regEx seems easy with strings, 
+//but jeeeez... with the special characters...
+
+
+//speaking of regEx with strings
+//lets clean up those phrases so when we print them, they'll be right!
+
+////we can put'em in an array
+//var trimmedSongs = [];
+//
+//for (var i = 0; i < cleanSongs.length; i++) {
+//	var trimmedSong = cleanSongs[i]
+//		.replace(/ - by | on the album /g, " ");
+//		trimmedSongs.push(trimmedSong);
+//}
+//
+//console.log(trimmedSongs);
+//
+////Must add each string to the DOM in index.html in the main content area.
+//
+//that was a dead end.
+
+
+//william caldwell had this idea (which i think is almost perfect) but 
+//we're gonna need those extra substrings.
+
+//first we'll need to target an element in the DOM
+
+var songsDiv = document.getElementById("song-names-panel");
+
+for (var i = 0; i < cleanSongs.length; i++) {
+    
+    var firstSplit = cleanSongs[i].split(" - by ");
+	console.log('firstSplit:', firstSplit);
+
+//returns an array with what was before " - by " 
+//and what was after (at current index item) ["song name", "the rest of the string that was cleanSongs[i]"]
+//firstSplit[0] is songName
+
+    var songName = firstSplit[0];
+
+//whats left is split at the the substring " on the album "
+//which return the array secondSplit ["artist", "album"]
+	
+    var secondSplit = firstSplit[1].split(" on the album ");
+
+//secondSplit's elements are plugged in to their nice little variables
+
+    var artist = secondSplit[0];
+    var album = secondSplit[1];
+
+//and then we print, incrementally setting innerHTML
+
+songsDiv.innerHTML += `<h4> ${songName} </h4>
+					   <p> ${artist} | ${album} | music </p>`;					   
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
